6df7436f01d9bd06b6a35008cf5b18fea4c729c5
Fred Warsaw <fred.warsaw@yahoo.com>
2018-11-13 16:52:47 -0600

    Second Commit

---
+++ NeoService/src/main/java/com/fredwarsaw/service/neo/model/Links.java
@@ -0,0 +1,16 @@
+package com.fredwarsaw.service.neo.model;
+
+public class Links {
+
+	private String next;
+    private String prev;
+
+    public String getNext() {
+        return next;
+    }
+
+    public String getPrev() {
+        return prev;
+    }
+
+}
---
+++ NeoService/src/main/java/com/fredwarsaw/service/neo/rest/Links.java
@@ -0,0 +1,16 @@
+package com.fredwarsaw.service.neo.rest;
+
+public class Links {
+
+	private String next;
+    private String prev;
+
+    public String getNext() {
+        return next;
+    }
+
+    public String getPrev() {
+        return prev;
+    }
+
+}
